5bf712daedf5565a0831f6351531602f44a007aa
irungumaina <kirathimo2010@gmail.com>
2019-05-31 12:26:01 +0300

    set time limits for the program

--- website_blocker.py
+++ website_blocker.py
@@ -1,8 +1,12 @@
 import time
+from datetime import datetime as dt
 hosts_path = r"/etc/hosts"
 redirect = "127.0.0.1"
 website_list = ["www.facebook.com", "facebook.com"]
 
 while True:
-    print(1)
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 18):
+        print(" Working hours...........")
+    else:
+        print("Fun Hours .............")
     time.sleep(5)
